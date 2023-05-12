11b60875673d2e3d0555b03b73c2adfbff3e3bec
E. Hannah <echannah631@gmail.com>
2023-05-12 00:22:49 -0500

    refactored file and css classes, css grid and media queries

--- src/components/header.js
+++ src/components/header.js
@@ -1,31 +1,18 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import logo from "../images/logo-designcode.svg"
+import "../css/header.css"
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <div className='header'>
+    <div className='header-group'>
+      <Link to="/">
+        <img src={require('../images/logo-designcode.svg')} width="30" />
+      </Link>
+      <Link to="/courses">Courses</Link>
+      <Link to="/downloads">Downloads</Link>
+      <Link to="/workshops">Workshops</Link>
+      <Link to="/buy"><button>Buy</button></Link>
     </div>
   </div>
 )
