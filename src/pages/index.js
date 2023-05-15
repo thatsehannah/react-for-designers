bcbdeb7a387638e6a517f620269c034c3b0ff841
E. Hannah <echannah631@gmail.com>
2023-05-15 15:35:37 -0500

    added logos, learned how to animate svgs

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import sketchLogo from '../images/logo-sketch.png'
+import figmaLogo from '../images/logo-figma.png'
+import studioLogo from '../images/logo-studio.png'
+import framerLogo from '../images/logo-framer.png'
+import reactLogo from '../images/logo-react.png'
+import swiftLogo from '../images/logo-swift.png'
+
+
+//the values attribute in animate tag in the svg takes 3 "d" values from the 3 different SVGs to create the wave effect. The last "d" value is the original state (the first "d" value that's followed by a semicolon)
 
 const IndexPage = () => (
   <div>
@@ -8,6 +17,19 @@ const IndexPage = () => (
       <h1>Learn to <br /> design and code React apps</h1>
       <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
       <Link to="/page-2/">Watch the video</Link>
+      <div className='logos'>
+        <img src={sketchLogo} alt="sketch logo" width="50" />
+        <img src={figmaLogo} alt="sketch logo" width="50" />
+        <img src={studioLogo} alt="sketch logo" width="50" />
+        <img src={framerLogo} alt="sketch logo" width="50" />
+        <img src={reactLogo} alt="sketch logo" width="50" />
+        <img src={swiftLogo} alt="sketch logo" width="50" />
+      </div>
+      <svg width="100%" height="172" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path fill="white">
+        <animate repeatCount="indefinite" fill='freeze' attributeName='d' dur="10s" values="M0 25.9086C277 84.5821 433 65.736 720 25.9086C934.818 -3.9019 1214.06 -5.23669 1442 8.06597C2079 45.2421 2208 63.5007 2560 25.9088V171.91L0 171.91V25.9086Z; M0 87.1596C316 87.1597 444 160 884 52.0001C1324 -55.9999 1320.29 34.966 1538 71.251C1814 117.251 2156 189.252 2560 87.1597V233.161L0 233.161V87.1596Z; M0 53.6584C158 11.0001 213 0 363 0C513 0 855.555 115.001 1154 115.001C1440 115.001 1626 -38.0004 2560 53.6585V199.66L0 199.66V53.6584Z;M0 25.9086C277 84.5821 433 65.736 720 25.9086C934.818 -3.9019 1214.06 -5.23669 1442 8.06597C2079 45.2421 2208 63.5007 2560 25.9088V171.91L0 171.91V25.9086Z" /></path>
+</svg>
+
       </div>
     </div>
    
@@ -15,3 +37,4 @@ const IndexPage = () => (
 )
 
 export default IndexPage
+
