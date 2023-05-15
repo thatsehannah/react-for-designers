b8afc8d3ead65ee8fb3124abb8a264e8392d6857
E. Hannah <echannah631@gmail.com>
2023-05-15 18:06:20 -0500

    used react hooks to change state to add background to header when user scrolls

--- src/components/header.js
+++ src/components/header.js
@@ -1,22 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Link from 'gatsby-link'
 import logo from '../images/logo-designcode.svg'
 import '../css/header.css'
 
-const Header = ({ siteTitle }) => (
-  <div className="header">
-    <div className="header-group">
-      <Link to="/">
-        <img src={logo} width="30" />
-      </Link>
-      <Link to="/courses">Courses</Link>
-      <Link to="/downloads">Downloads</Link>
-      <Link to="/workshops">Workshops</Link>
-      <Link to="/buy">
-        <button>Buy</button>
-      </Link>
+const Header = () => {
+  const [scrollPosition, setScrollPosition] = useState(0)
+  const [hasScrolled, setHasScrolled] = useState(false)
+
+  useEffect(() => {
+    const onScroll = () => {
+      setScrollPosition(window.scrollY)
+    }
+    window.addEventListener('scroll', onScroll)
+    return () => window.removeEventListener('scroll', onScroll)
+  }, [])
+
+  useEffect(() => {
+    setHasScrolled(scrollPosition > 50)
+  }, [scrollPosition])
+
+  return (
+    <div className={hasScrolled ? 'header header-scroll' : 'header'}>
+      <div className="header-group">
+        <Link to="/">
+          <img src={logo} width="30" />
+        </Link>
+        <Link to="/courses">Courses</Link>
+        <Link to="/downloads">Downloads</Link>
+        <Link to="/workshops">Workshops</Link>
+        <Link to="/buy">
+          <button>Buy</button>
+        </Link>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default Header
