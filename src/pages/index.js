da7e65e062629a80424696eef915dab7ee92b4e5
E. Hannah <echannah631@gmail.com>
2023-05-18 14:03:59 -0500

    used json to create static data, created cell component for cell data

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import styled from 'styled-components'
 import sketchLogo from '../images/logo-sketch.png'
 import figmaLogo from '../images/logo-figma.png'
 import studioLogo from '../images/logo-studio.png'
@@ -9,8 +10,31 @@ import swiftLogo from '../images/logo-swift.png'
 import Card from '../components/Card'
 import Section from '../components/Section'
 import Wave from '../components/Wave'
+import staticData from '../../data.json'
+import Cell from '../components/Cell'
 
 //the values attribute in animate tag in the svg takes 3 "d" values from the 3 different SVGs to create the wave effect. The last "d" value is the original state (the first "d" value that's followed by a semicolon)
+
+const SectionCaption = styled.p`
+  font-weight: 600;
+  font-size: 18px;
+  text-transform: uppercase;
+  color: #94a4ba;
+  text-align: center;
+`
+
+const SectionCellGroup = styled.div`
+  max-width: 800px;
+  margin: 0 auto 100px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  column-gap: 20px;
+  padding: 0 20px;
+
+  @media (max-width: 800px) {
+    grid-template-columns: repeat(1, auto);
+  }
+`
 
 const IndexPage = () => (
   <div>
@@ -66,6 +90,12 @@ const IndexPage = () => (
       title="React for Designers"
       text="Learn how to build a modern site using React and the most efficient libraries to get your site/product online. Get familiar with components, CSS Grid, animations, interactions, dynamic data with Contentful and deploying your site with Netlify."
     />
+    <SectionCaption>12 sections - 6 hours</SectionCaption>
+    <SectionCellGroup>
+      {staticData.cells.map((cell) => (
+        <Cell title={cell.title} image={cell.image} />
+      ))}
+    </SectionCellGroup>
   </div>
 )
 
